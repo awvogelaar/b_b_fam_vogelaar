3a6ae6026be83f158cb8ed79694e476193cb3db2
A.W. Vogelaar <awvogelaar@outlook.com>
2025-12-09 01:47:52 +0100

    ready for deploy3

--- bedandbreakfast/settings.py
+++ bedandbreakfast/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "bed-and-breakfast-vogelaar.onrender.com")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "bed-and-breakfast-vogelaar.onrender.com").split(",")
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "AdianVogelaarIsAdmin!")
 
 INSTALLED_APPS = [
